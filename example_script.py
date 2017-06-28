bbc6a5862ff3ed5bb2b2004916d457ca6b17c23e
Nicolas Brown <nic.brown0@gmail.com>
2017-06-28 17:11:33 -0400

    Create example_script.py

---
+++ example_script.py
@@ -0,0 +1,14 @@
+# Define directories
+# Clip polygons directory
+
+# Raster directory
+
+# Output directory
+
+
+# Loop through all clipping polygons
+# Execute clip
+
+# Calculate solar radiation
+
+# Aply SolarTools- Calibrate solar potential
